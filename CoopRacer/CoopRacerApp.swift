4868f17b83fd3f68778ccc5bdec2bec6f68faf2a
Ashutosh Bhardwaj <Ashutosh.bhardwaj@gmail.com>
2025-09-12 23:13:05 -0400

    Initial Commit

---
+++ CoopRacer/CoopRacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoopRacerApp.swift
+//  CoopRacer
+//
+//  Created by Ashutosh Bhardwaj on 2025-09-12.
+//
+
+import SwiftUI
+
+@main
+struct CoopRacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoopRacerTests/CoopRacerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoopRacerTests.swift
+//  CoopRacerTests
+//
+//  Created by Ashutosh Bhardwaj on 2025-09-12.
+//
+
+import Testing
+@testable import CoopRacer
+
+struct CoopRacerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
